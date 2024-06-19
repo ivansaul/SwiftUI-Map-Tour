e083760b5983acb7d7971414b0164131093149d0
ivansaul <saul_2503@hotmail.com>
2024-06-19 10:45:25 -0500

    Initial Commit

---
+++ SwiftUIMapTour/SwiftUIMapTourApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMapTourApp.swift
+//  SwiftUIMapTour
+//
+//  Created by saul on 6/19/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMapTourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
